59143a71334d9c6b3f98ee6f715f3c9ccb6d5f1d
Anthony <anthonynarlock@outlook.com>
2021-09-18 07:02:29 -0500

    Update RecursionPractice.java

--- Java Review/RecursionPractice.java
+++ Java Review/RecursionPractice.java
@@ -6,6 +6,8 @@
 *
 * When we do Recursion, we want to always know when the stopping point is.
 * And, what is the easiest way to do the problem assigned?
+*
+* All recursive questions will have a base case - what will end the recursive calls
 */
 
 public class RecursionPractice() {
@@ -74,4 +76,52 @@ public class RecursionPractice() {
         }
       }
 
+
+    /*
+    * triangle
+    * We have triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks,
+    * the next row has 3 blocks, and so on. Compute recursively (no loops or multiplication)
+    * the total number of blocks in such a triangle with the given number of rows.
+    */
+
+    public int triangle(int rows) {
+      if (rows == 0) {
+        return rows;
+      }
+      
+      return rows + triangle(rows - 1);
+    }
+
+    /*
+    * sumDigits
+    * Given a non-negative int n, return the sum of its digits recursively (no loops). Note that
+    * mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes
+    * the rightmost digit (126 / 10 is 12)
+    */
+
+    public int sumDigits(int n) {
+      if(n / 10 < 1) {
+        return n;
+      }
+      
+      return (n % 10) + sumDigits(n / 10);
+    }
+    
+    /*
+    * count7
+    * Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for
+    * example 717 yields 2. (no loops). Note that mod (%) by 10 yields the rightmost digit
+    * (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12)
+    */
+
+    public int count7(int n) {
+      if(n % 10 == 7) {
+        return 1 + count7(n / 10);
+      }
+      if (n / 10 < 1) {
+        return 0;
+      } else {
+        return count7(n / 10);
+      }
+    }
 }
